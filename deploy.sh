ea12861057edee960ab362fdac5528344f05b88d
VictorCampos93 <v.hugo93@hotmail.com>
2020-08-02 21:07:03 -0300

    arrumando o script de deploy

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,15 @@ docker build -t victorcampo5/multi-client:latest -t victorcampo5/multi-client:$S
 docker build -t victorcampo5/multi-server:latest -t victorcampo5/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t victorcampo5/multi-worker:latest -t victorcampo5/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-dockerpush victorcampo5/multi-client:latest
-dockerpush victorcampo5/multi-server:latest
-dockerpush victorcampo5/multi-worker:latest
+docker push victorcampo5/multi-client:latest
+docker push victorcampo5/multi-server:latest
+docker push victorcampo5/multi-worker:latest
 
-dockerpush victorcampo5/multi-client:$SHA
-dockerpush victorcampo5/multi-server:$SHA
-dockerpush victorcampo5/multi-worker:$SHA
+docker push victorcampo5/multi-client:$SHA
+docker push victorcampo5/multi-server:$SHA
+docker push victorcampo5/multi-worker:$SHA
 
-docker apply -f k8s
+kubectl apply -f k8s
 
 kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
 kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
